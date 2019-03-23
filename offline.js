7df41f6a8b72771cd2ad1394d643bff7795afde9
TttooohhhkAAAA <ants8625@gmail.com>
2019-03-23 17:28:05 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1553351219,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/9patch.png",
+		"images/sprite-sheet0.png",
+		"images/9patch2.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/chipshandle3.ogg",
+		"media/chipsstack3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
